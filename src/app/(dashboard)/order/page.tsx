c123a7f26f202744448855dd47c85beff1947ad9
Pian <alifalpian157@gmail.com>
2025-09-19 19:48:53 +0700

    Order List

---
+++ src/app/(dashboard)/order/page.tsx
@@ -0,0 +1,9 @@
+import OrderManagement from "./_components/order"
+
+export const metadata = {
+  title: "WPU Caffee - Table Management",
+};
+
+export default function MenuManagementPage() {
+  return <OrderManagement />;
+}
